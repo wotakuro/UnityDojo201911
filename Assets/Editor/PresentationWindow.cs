c945016892b7629337e95b0ee1ec325fa35f64e1
wotakuro <wotakuro@gmail.com>
2019-10-30 21:26:14 +0900

    update

--- Assets/Editor/PresentationWindow.cs
+++ Assets/Editor/PresentationWindow.cs
@@ -13,49 +13,118 @@ public class PresentationWindow : EditorWindow
         EditorWindow.GetWindow<PresentationWindow>();
     }
 
-    private Rect oldRect = new Rect();
+    private VisualElement root;
+    private VisualElement currentElement;
+    private Rect currentWindowRect = new Rect();
+
+    private int currentPage;
+    
 
     private void OnEnable()
     {
         Reload();
     }
-    VisualElement root;
 
     private void Reload()
     {
+        Debug.Log("Reload!!!");
+        currentWindowRect = new Rect(0,0,0,0);
         this.rootVisualElement.Clear();
         string path = "Assets/Editor/Slides/Presentation.uxml";
         var asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
         root = asset.CloneTree();
 
-        this.rootVisualElement.Add(root);        
+        // register Reload Button
+        root.Query<Button>("Reload").ForEach((button) =>
+        {
+            button.clickable.clicked += Reload;
+        });
+        this.rootVisualElement.Add(root);
+        // nextPage
+        root.Query<Button>("NextPage").ForEach((button) =>
+        {
+            button.clickable.clicked += ()=>{
+                this.currentPage++;
+                ChangePageNumber();
+            };
+        });
+
+        // nextPage
+        root.Query<Button>("PrevPage").ForEach((button) =>
+        {
+            button.clickable.clicked += () => {
+                this.currentPage--;
+                ChangePageNumber();
+            };
+        });
+        // label
+        this.rootVisualElement.Add(root);
     }
+
+    private List<VisualTreeAsset> GetSlideAssets()
+    {
+        List<VisualTreeAsset> assets = new List<VisualTreeAsset>();
+        var guids = AssetDatabase.FindAssets("t:VisualTreeAsset Page");
+        foreach( var guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            var asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+            assets.Add(asset);
+        }
+        return assets;
+    }
+
+    private void ChangePageNumber()
+    {
+        var assets = GetSlideAssets();
+
+        if( currentElement != null)
+        {
+            currentElement.parent.Remove(currentElement);
+        }
+
+        currentElement = assets[currentPage-1].CloneTree();
+
+        ScalePage(this.currentWindowRect, currentElement);
+        root.Insert(0, currentElement);
+        root.Query<Label>("PageNumber").ForEach((label) =>
+        {
+            label.text = this.currentPage.ToString();
+        });
+    }
+
 
     public void Update()
     {
         var rect = this.rootVisualElement.contentRect;
 
-        if(oldRect == rect)
+        if(currentWindowRect == rect)
         {
             return;
         }
-        var labels = this.rootVisualElement.Query<Label>().
-            ForEach((element) =>
-            {
-                if(element.userData == null)
-                {
-                    element.userData = element.resolvedStyle.fontSize;
-                }
-                float originSize = (float)element.userData;
-                element.style.fontSize = originSize * rect.width / 960.0f;
-                Debug.Log("originFont " + originSize + "->" + element.style.fontSize);
-                element.MarkDirtyRepaint();
-                return element;
-            });
-        
+        if(float.IsNaN( rect.width ) || float.IsNaN(rect.height))
+        {
+            currentWindowRect = rect;
+            return;
+        }
 
+        // page scaling
+        ScalePage(rect, this.rootVisualElement);
         root.style.width = rect.width;
         root.style.height = rect.height;
-        oldRect = rect;
+        currentWindowRect = rect;
+    }
+    // page scaling
+    private void ScalePage(Rect rect,VisualElement visualElement)
+    {
+        visualElement.Query<VisualElement>(null, "page").
+            ForEach((element) =>
+            {
+                if (rect.width == float.NaN || rect.height == float.NaN)
+                {
+                    return;
+                }
+                element.transform.scale = new Vector3((float)rect.width / 960.0f, (float)rect.height / 540.0f, 1);
+            });
     }
 }
