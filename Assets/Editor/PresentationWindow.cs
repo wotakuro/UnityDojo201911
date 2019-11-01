523356d77bc9dcb4ffb135628a9ea484335570fe
wotakuro <wotakuro@gmail.com>
2019-11-01 19:38:09 +0900

    update

--- Assets/Editor/PresentationWindow.cs
+++ Assets/Editor/PresentationWindow.cs
@@ -5,126 +5,185 @@ using UnityEditor;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 
-public class PresentationWindow : EditorWindow
+namespace UTJ
 {
-    [MenuItem("Tools/Presentation")]
-    public static void Create()
+    public class PresentationWindow : EditorWindow
     {
-        EditorWindow.GetWindow<PresentationWindow>();
-    }
-
-    private VisualElement root;
-    private VisualElement currentElement;
-    private Rect currentWindowRect = new Rect();
-
-    private int currentPage;
-    
-
-    private void OnEnable()
-    {
-        Reload();
-    }
-
-    private void Reload()
-    {
-        Debug.Log("Reload!!!");
-        currentWindowRect = new Rect(0,0,0,0);
-        this.rootVisualElement.Clear();
-        string path = "Assets/Editor/Slides/Presentation.uxml";
-        var asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
-        root = asset.CloneTree();
-
-        // register Reload Button
-        root.Query<Button>("Reload").ForEach((button) =>
+        [MenuItem("Tools/Presentation")]
+        public static void Create()
         {
-            button.clickable.clicked += Reload;
-        });
-        this.rootVisualElement.Add(root);
-        // nextPage
-        root.Query<Button>("NextPage").ForEach((button) =>
+            EditorWindow.GetWindow<PresentationWindow>();
+        }
+
+        private VisualElement root;
+        private VisualElement currentElement;
+        private Rect currentWindowRect = new Rect();
+        private List<VisualTreeAsset> pageAssets;
+
+        private int currentPage;
+
+
+        private void OnEnable()
         {
-            button.clickable.clicked += ()=>{
+            Reload();
+        }
+
+        private void Reload()
+        {
+            currentPage = 0;
+            currentWindowRect = new Rect(0, 0, 0, 0);
+            this.rootVisualElement.Clear();
+            string path = "Assets/Editor/Slides/Presentation.uxml";
+            var asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+            root = asset.CloneTree();
+
+            // register Reload Button
+            root.Query<Button>("Reload").ForEach((button) =>
+            {
+                button.clickable.clicked += Reload;
+            });
+            this.rootVisualElement.Add(root);
+            // nextPage
+            root.Query<Button>("NextPage").ForEach((button) =>
+            {
+                button.clickable.clicked += () =>
+                {
+                    this.NextPage();
+                };
+            });
+            // nextPage
+            root.Query<Button>("PrevPage").ForEach((button) =>
+            {
+                button.clickable.clicked += () =>
+                {
+                    this.PrevPage();
+                };
+            });
+            root.focusable = true;
+            root.RegisterCallback<KeyDownEvent>((evt) =>
+            {
+                if( evt.keyCode == KeyCode.RightArrow)
+                {
+                    this.NextPage();
+                }else if( evt.keyCode == KeyCode.LeftArrow)
+                {
+                    this.PrevPage();
+                }
+            });
+            this.rootVisualElement.Add(root);
+            // load first page
+            this.pageAssets = GetSlideAssets();
+            ChangePageNumber();
+        }
+        void NextPage()
+        {
+            if (this.currentPage < this.pageAssets.Count - 1)
+            {
                 this.currentPage++;
                 ChangePageNumber();
-            };
-        });
-
-        // nextPage
-        root.Query<Button>("PrevPage").ForEach((button) =>
+            }
+        }
+        void PrevPage()
         {
-            button.clickable.clicked += () => {
+            if (this.currentPage > 0)
+            {
                 this.currentPage--;
                 ChangePageNumber();
-            };
-        });
-        // label
-        this.rootVisualElement.Add(root);
-    }
-
-    private List<VisualTreeAsset> GetSlideAssets()
-    {
-        List<VisualTreeAsset> assets = new List<VisualTreeAsset>();
-        var guids = AssetDatabase.FindAssets("t:VisualTreeAsset Page");
-        foreach( var guid in guids)
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid);
-            var asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
-            assets.Add(asset);
-        }
-        return assets;
-    }
-
-    private void ChangePageNumber()
-    {
-        var assets = GetSlideAssets();
-
-        if( currentElement != null)
-        {
-            currentElement.parent.Remove(currentElement);
+            }
         }
 
-        currentElement = assets[currentPage-1].CloneTree();
-
-        ScalePage(this.currentWindowRect, currentElement);
-        root.Insert(0, currentElement);
-        root.Query<Label>("PageNumber").ForEach((label) =>
+        private List<VisualTreeAsset> GetSlideAssets()
         {
-            label.text = this.currentPage.ToString();
-        });
-    }
-
-
-    public void Update()
-    {
-        var rect = this.rootVisualElement.contentRect;
-
-        if(currentWindowRect == rect)
-        {
-            return;
-        }
-        if(float.IsNaN( rect.width ) || float.IsNaN(rect.height))
-        {
-            currentWindowRect = rect;
-            return;
-        }
-
-        // page scaling
-        ScalePage(rect, this.rootVisualElement);
-        root.style.width = rect.width;
-        root.style.height = rect.height;
-        currentWindowRect = rect;
-    }
-    // page scaling
-    private void ScalePage(Rect rect,VisualElement visualElement)
-    {
-        visualElement.Query<VisualElement>(null, "page").
-            ForEach((element) =>
+            List<VisualTreeAsset> assets = new List<VisualTreeAsset>();
+            var guids = AssetDatabase.FindAssets("t:VisualTreeAsset Page");
+            foreach (var guid in guids)
             {
-                if (rect.width == float.NaN || rect.height == float.NaN)
-                {
-                    return;
-                }
-                element.transform.scale = new Vector3((float)rect.width / 960.0f, (float)rect.height / 540.0f, 1);
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                var asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+                assets.Add(asset);
+            }
+            return assets;
+        }
+
+        private void ChangePageNumber()
+        {
+
+            if (currentElement != null)
+            {
+                currentElement.parent.Remove(currentElement);
+            }
+
+            currentElement = pageAssets[currentPage ].CloneTree();
+
+            ScalePage(this.currentWindowRect, currentElement);
+            root.Insert(0, currentElement);
+            root.Query<Label>("PageNumber").ForEach((label) =>
+            {
+                label.text = (this.currentPage + 1).ToString();
             });
+            //
+            root.Query<Button>("NextPage").ForEach((button) =>
+            {
+                button.visible = (this.currentPage < this.pageAssets.Count - 1);
+            });
+
+            //
+            root.Query<Button>("PrevPage").ForEach((button) =>
+            {
+                button.visible = (this.currentPage > 0 );
+            });
+        }
+
+        public void Update()
+        {
+            var rect = this.rootVisualElement.contentRect;
+
+            if (currentWindowRect == rect)
+            {
+                return;
+            }
+            if (float.IsNaN(rect.width) || float.IsNaN(rect.height))
+            {
+                currentWindowRect = rect;
+                return;
+            }
+
+            // page scaling
+            ScalePage(rect, this.rootVisualElement);
+            root.style.width = rect.width;
+            root.style.height = rect.height;
+            currentWindowRect = rect;
+        }
+        // page scaling
+        private void ScalePage(Rect rect, VisualElement visualElement)
+        {
+            const float expectedWidth = 960.0f;
+            const float expectedHeight = 540.0f;
+            const float expectedAspect = expectedWidth / expectedHeight;
+            visualElement.Query<VisualElement>(null, "page").
+                ForEach((element) =>
+                {
+                    if (rect.width == float.NaN || rect.height == float.NaN)
+                    {
+                        return;
+                    }
+                    Vector2 offset = new Vector2();
+                    float scaleRate = (float)rect.width / expectedWidth;
+                    if ((float)rect.width / (float)rect.height < expectedAspect)
+                    {
+                        // 縦長
+                        scaleRate = (float)rect.width / expectedWidth;
+                        offset.y = (rect.height - expectedHeight * scaleRate) * 0.5f;
+                    }
+                    else
+                    {
+                        // 横長
+                        scaleRate = (float)rect.height / expectedHeight;
+                        offset.x = (rect.width - expectedWidth * scaleRate) * 0.5f;
+                    }
+                    element.transform.position = new Vector3(offset.x, offset.y, 0.0f);
+                    element.transform.scale = new Vector3(scaleRate, scaleRate, 1);
+                });
+        }
     }
 }
