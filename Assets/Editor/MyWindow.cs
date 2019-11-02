34abe38efdaaf1eb1d82b8a69da1a63774b208d2
wotakuro <wotakuro@gmail.com>
2019-11-02 19:44:57 +0900

    update

---
+++ Assets/Editor/MyWindow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+class MyWindow : EditorWindow
+{
+    [MenuItem("Tools/Sample")]
+    public static void Create()
+    {
+        EditorWindow.GetWindow<MyWindow>();
+    }
+    // 描画はC#で全部書く
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("テキスト表示");
+        if (GUILayout.Button("決定"))
+        {
+            // 何か処理する
+        }
+    }
+}
---
+++ Assets/Editor/Samples/001/NewUICs.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+public class NewUICs : EditorWindow
+{
+    [MenuItem("Samples/001")]
+    public static void Create()
+    {
+        EditorWindow.GetWindow<NewUICs>();
+    }
+    void OnEnable()
+    {
+        // uxml 読み込み
+        string path = "Assets/Editor/Samples/001/NewUI.uxml";
+        var asset = 
+            AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+        // 読み込んだUXMLをEditorWindowに配置
+        asset.CloneTree(this.rootVisualElement);
+    }
+}
